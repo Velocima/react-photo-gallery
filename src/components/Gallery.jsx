c1da27b0a61cc7eaa5ec84df7e36641f28eb96f5
Velo <hartley.max@outlook.com>
2020-12-02 22:08:57 +0000

    images renamed and imported into gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,5 +1,21 @@
 import React, { useEffect } from 'react';
+import wallpaper1 from '../images/wallpaper1.jpg';
+import wallpaper2 from '../images/wallpaper2.jpg';
+import wallpaper3 from '../images/wallpaper3.jpg';
+import wallpaper4 from '../images/wallpaper4.jpg';
+import wallpaper5 from '../images/wallpaper5.jpg';
+import wallpaper6 from '../images/wallpaper6.jpg';
+import wallpaper7 from '../images/wallpaper7.jpg';
+import wallpaper8 from '../images/wallpaper8.jpg';
+import wallpaper9 from '../images/wallpaper9.jpg';
+import wallpaper10 from '../images/wallpaper10.jpg';
+import wallpaper11 from '../images/wallpaper11.jpg';
+import wallpaper12 from '../images/wallpaper12.jpg';
 
 export default function Gallery() {
-	return <>Gallery</>;
+	return (
+		<>
+			<img src={wallpaper1} alt='' />
+		</>
+	);
 }
